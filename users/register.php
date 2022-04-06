25d272ebe358e76ffed6d2b4a47172235b173add
EbuCodes <ebucodes@gmail.com>
2022-04-06 18:58:56 +0100

    commit

--- users/register.php
+++ users/register.php
@@ -8,12 +8,10 @@
     <meta name="generator" content="Hugo 0.84.0">
     <title>EbuCodes · Register</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/checkout/">
-
-    
-
     <!-- Bootstrap core CSS -->
     <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="../assets/images/favicon1.png">
 
     <style>
       .bd-placeholder-img {
@@ -48,11 +46,11 @@
     <div class="row g-5">
       <div class="col-md-12">
         <!-- <h4 class="mb-3"></h4> -->
-        <form class="needs-validation" novalidate>
+        <form method="POST" class="needs-validation" novalidate>
           <div class="row g-3">
             <div class="col-sm-6">
               <label for="firstName" class="form-label">First name</label>
-              <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+              <input type="text" class="form-control" name="firstName" id="firstName" placeholder="Your first name" value="" required>
               <div class="invalid-feedback">
                 Valid first name is required.
               </div>
@@ -60,84 +58,41 @@
 
             <div class="col-sm-6">
               <label for="lastName" class="form-label">Last name</label>
-              <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
+              <input type="text" class="form-control" name="lastName" id="lastName" placeholder="Your last name" value="" required>
               <div class="invalid-feedback">
                 Valid last name is required.
               </div>
             </div>
 
-            <div class="col-12">
-              <label for="username" class="form-label">Username</label>
-              <div class="input-group has-validation">
-                <span class="input-group-text">@</span>
-                <input type="text" class="form-control" id="username" placeholder="Username" required>
+            <div class="col-sm-6">
+              <label for="email" class="form-label">Email</label>
+              <input type="email" class="form-control" id="email" placeholder="Your email address" required>
               <div class="invalid-feedback">
-                  Your username is required.
-                </div>
+                Please enter a valid email address.
               </div>
             </div>
 
-            <div class="col-12">
-              <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
-              <input type="email" class="form-control" id="email" placeholder="you@example.com">
+            <div class="col-sm-6">
+              <label for="address" class="form-label">Password</label>&nbsp;<span style="color: red;">*</span>
+              <input type="password" class="form-control" id="address" minlength="5" maxlength="12" placeholder="Your password" required>
               <div class="invalid-feedback">
-                Please enter a valid email address for shipping updates.
+                Password is required.<br>
+                Minimum length is 5.<br>
+                Maximum length is 12.
               </div>
             </div>
+         
+        </div>
 
-            <div class="col-12">
-              <label for="address" class="form-label">Address</label>
-              <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
-              <div class="invalid-feedback">
-                Please enter your shipping address.
-              </div>
-            </div>
+          <br class="my-2">
 
-            <div class="col-12">
-              <label for="address2" class="form-label">Address 2 <span class="text-muted">(Optional)</span></label>
-              <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
-            </div>
-
-            <div class="col-md-5">
-              <label for="country" class="form-label">Country</label>
-              <select class="form-select" id="country" required>
-                <option value="">Choose...</option>
-                <option>United States</option>
-              </select>
-              <div class="invalid-feedback">
-                Please select a valid country.
-              </div>
-            </div>
-
-            <div class="col-md-4">
-              <label for="state" class="form-label">State</label>
-              <select class="form-select" id="state" required>
-                <option value="">Choose...</option>
-                <option>California</option>
-              </select>
-              <div class="invalid-feedback">
-                Please provide a valid state.
-              </div>
-            </div>
-
-            <div class="col-md-3">
-              <label for="zip" class="form-label">Zip</label>
-              <input type="text" class="form-control" id="zip" placeholder="" required>
-              <div class="invalid-feedback">
-                Zip code required.
-              </div>
-            </div>
-          </div>
-
-          <br class="my-4">
-
-          <button class="w-100 btn btn-primary btn-lg" type="submit">Register</button>
+          <button name="register" class="w-100 btn btn-primary btn-lg" type="submit">Register</button>
         </form>
       </div>
     </div>
   </main>
 
-  <footer class="my-5 pt-5 text-muted text-center text-small">
+  <footer class="my-2 pt-5 text-muted text-center text-small">
     <p class="mb-1">&copy; <?php echo date('Y'); ?></p>
   </footer>
 </div>
